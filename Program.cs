0cd23139cfe3eba714ca75ae02ee17b6606598bc
vasanthcad01 <vasanthancad01@gmail.com>
2024-02-19 16:36:42 +0530

    csharp method

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static int add(int a, int b)
+   {
+      return a+b;
+   }
+    static int sub(int a, int b)
+    {
+        return a-b;
+    }
+    static int mul(int a, int b)
+    {
+        return a*b;
+
+    }
+    static int quo(int a, int b)
+    {
+        return a/b;    
+    }
+    static int rem(int a , int b)
+    {
+        return a|b;
+
+    }
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine()); 
+        int b = Convert.ToInt32(Console.ReadLine()); 
+        
+        
+         Console.WriteLine(add(a,b));
+         Console.WriteLine(sub(a,b));
+         Console.WriteLine(mul(a,b));
+         Console.WriteLine(quo(a,b));
+         Console.WriteLine(rem(a,b));
+
+    }    
+    }
